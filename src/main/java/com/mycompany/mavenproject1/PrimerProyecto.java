187c685116ba12366ef1f942f95df1121fb13df2
Carlos Aranda <aranda.carlos.damian@gmail.com>
2022-08-08 16:02:46 -0300

    primer Proyecto Terminado

---
+++ src/main/java/com/mycompany/mavenproject1/PrimerProyecto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+import com.mycompany.mavenproject1.forms.Formulario;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PrimerProyecto {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
